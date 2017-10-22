b96afe6c58952be723c73cd23c49b68e147d8bdb
Elliot Shohet <elliot.shohet@gmail.com>
2017-10-22 14:36:46 -0700

    Deploy to rinkeby

--- truffle.js
+++ truffle.js
@@ -1,12 +1,15 @@
 // Allows us to use ES6 in our migrations and tests.
 require('babel-register');
+var HDWalletProvider = require("truffle-hdwallet-provider");
 
+const mnemonic = "kjweekwjeqlkewejqlkj two three four five six seven eight nine ten eleven twelve";
+var provider = new HDWalletProvider(mnemonic, "https://rinkeby.infura.io", 0);
+console.log(provider.getAddress());
 module.exports = {
-  networks: {
-    development: {
-      host: 'localhost',
-      port: 8545,
-      network_id: '*' // Match any network id
+    networks: {
+        "rinkeby": {
+            network_id: 4,    // Official ropsten network id
+            provider: provider, // Use our custom provider
+        }
     }
-  }
 };
